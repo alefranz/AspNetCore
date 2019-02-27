32beccb60aaadd785850e82bdf7d203e2658a3d3
Alessio Franceschelli <alessio@franceschelli.me>
2019-02-27 00:17:29 +0000

    DelegatingHandler Tests

--- src/Middleware/HeaderPropagation/test/HeaderPropagationMessageHandlerTest.cs
+++ src/Middleware/HeaderPropagation/test/HeaderPropagationMessageHandlerTest.cs
@@ -1,10 +1,151 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Xunit;
 
 namespace Microsoft.AspNetCore.HeaderPropagation.Tests
 {
-    class HeaderPropagationMessageHandlerTest
+    public class HeaderPropagationMessageHandlerTest
     {
+        public HeaderPropagationMessageHandlerTest()
+        {
+            Handler = new SimpleHandler();
+
+            State = new HeaderPropagationState();
+            Options = new HeaderPropagationOptions();
+
+            var headerPropagationMessageHandler =
+                new HeaderPropagationMessageHandler(new OptionsWrapper<HeaderPropagationOptions>(Options), State)
+                {
+                    InnerHandler = Handler
+                };
+
+            Client = new HttpClient(headerPropagationMessageHandler)
+            {
+                BaseAddress = new Uri("http://example.com")
+            };
+        }
+
+        private SimpleHandler Handler { get; }
+        private HeaderPropagationEntry Configuration { get; }
+        public HeaderPropagationState State { get; set; }
+        public HeaderPropagationOptions Options { get; set; }
+        public HttpClient Client { get; set; }
+
+        [Fact]
+        public async Task HeaderInState_AddCorrectValue()
+        {
+            // Arrange
+            Options.Headers.Add(new HeaderPropagationEntry { InputName = "in", OutputName = "out" });
+            State.Headers.Add("in", "test");
+            
+            // Act
+            await Client.SendAsync(new HttpRequestMessage());
+
+            // Assert
+            Assert.True(Handler.Headers.Contains("out"));
+            Assert.Equal(new[] { "test" }, Handler.Headers.GetValues("out"));
+        }
+
+        [Fact]
+        public async Task NoHeaderInState_DoesNotAddIt()
+        {
+            // Arrange
+            Options.Headers.Add(new HeaderPropagationEntry { InputName = "in", OutputName = "out" });
+
+            // Act
+            await Client.SendAsync(new HttpRequestMessage());
+
+            // Assert
+            Assert.Empty(Handler.Headers);
+        }
+
+        [Fact]
+        public async Task HeaderInState_NotInOptions_DoesNotAddIt()
+        {
+            // Arrange
+            Options.Headers.Add(new HeaderPropagationEntry { InputName = "no", OutputName = "out" });
+            State.Headers.Add("in", "test");
+
+            // Act
+            await Client.SendAsync(new HttpRequestMessage());
+
+            // Assert
+            Assert.Empty(Handler.Headers);
+        }
+
+        [Fact]
+        public async Task MultipleHeadersInState_AddsAll()
+        {
+            // Arrange
+            Options.Headers.Add(new HeaderPropagationEntry { InputName = "in", OutputName = "out" });
+            Options.Headers.Add(new HeaderPropagationEntry { InputName = "another", OutputName = "something" });
+            State.Headers.Add("in", "test");
+            State.Headers.Add("another", "test2");
+
+            // Act
+            await Client.SendAsync(new HttpRequestMessage());
+
+            // Assert
+            Assert.True(Handler.Headers.Contains("out"));
+            Assert.True(Handler.Headers.Contains("something"));
+            Assert.Equal(new[] { "test" }, Handler.Headers.GetValues("out"));
+            Assert.Equal(new[] { "test2" }, Handler.Headers.GetValues("something"));
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public async Task HeaderEmptyInState_DoNotAddIt(string headerValue)
+        {
+            // Arrange
+            Options.Headers.Add(new HeaderPropagationEntry { InputName = "in", OutputName = "out" });
+            State.Headers.Add("in", headerValue);
+
+            // Act
+            await Client.SendAsync(new HttpRequestMessage());
+
+            // Assert
+            Assert.False(Handler.Headers.Contains("out"));
+        }
+
+        [Theory]
+        [InlineData(false, "", new[] { "" })]
+        [InlineData(false, null, new[] { "" })]
+        [InlineData(false, "42", new[] { "42" })]
+        [InlineData(true, "42", new[] { "42", "test" })]
+        [InlineData(true, "", new[] { "", "test" })]
+        [InlineData(true, null, new[] { "", "test" })]
+        public async Task HeaderInState_HeaderAlreadyInOutgoingRequest(bool alwaysAdd, string outgoingValue, string[] expectedValues)
+        {
+            // Arrange
+            State.Headers.Add("in", "test");
+            Options.Headers.Add(
+                new HeaderPropagationEntry {InputName = "in", OutputName = "out", AlwaysAdd = alwaysAdd});
+
+            var request = new HttpRequestMessage();
+            request.Headers.Add("out", outgoingValue);
+
+            // Act
+            await Client.SendAsync(request);
+
+            // Assert
+            Assert.True(Handler.Headers.Contains("out"));
+            Assert.Equal(expectedValues, Handler.Headers.GetValues("out"));
+        }
+
+        private class SimpleHandler : DelegatingHandler
+        {
+            public HttpHeaders Headers { get; private set; }
+
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                Headers = request.Headers;
+                return Task.FromResult(new HttpResponseMessage());
+            }
+        }
     }
 }
