4cc2b15e8767c43afa2a7e0b5ea638c4d16231f0
Alessio Franceschelli <alessio@franceschelli.me>
2019-12-08 18:05:04 +0000

    Fair benchmark

--- src/Middleware/perf/ResponseCaching.Microbenchmarks/ResponseCachingBenchmark.cs
+++ src/Middleware/perf/ResponseCaching.Microbenchmarks/ResponseCachingBenchmark.cs
@@ -2,10 +2,14 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipelines;
+using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.ResponseCaching;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.ObjectPool;
@@ -22,9 +26,9 @@ namespace Microsoft.AspNetCore.WebSockets.Microbenchmarks
         private readonly byte[] _data = new byte[100 * 1024 * 1024];
 
         [Params(
-            100,
-            64 * 1024,
-            100 * 1024 * 1024
+            //100,
+            //64 * 1024,
+            1 * 1024 * 1024
         )]
         public int Size { get; set; }
 
@@ -49,26 +53,82 @@ namespace Microsoft.AspNetCore.WebSockets.Microbenchmarks
         }
 
         //[Benchmark]
-        //public async Task Cache()
-        //{
-        //    var context = new DefaultHttpContext();
-        //    context.Request.Method = HttpMethods.Get;
-        //    context.Request.Path = "/a";
+        public async Task Cache()
+        {
+            var context = new DefaultHttpContext();
+            context.Request.Method = HttpMethods.Get;
+            context.Request.Path = "/a";
 
-        //    // don't serve from cache but store result
-        //    context.Request.Headers[HeaderNames.CacheControl] = CacheControlHeaderValue.NoCacheString;
+            // don't serve from cache but store result
+            context.Request.Headers[HeaderNames.CacheControl] = CacheControlHeaderValue.NoCacheString;
 
-        //    await _middleware.Invoke(context);
-        //}
+            await _middleware.Invoke(context);
+        }
 
         [Benchmark]
         public async Task ServeFromCache()
         {
-            var context = new DefaultHttpContext();
+            var pipe = new Pipe();
+            var consumer = ConsumeAsync(pipe.Reader, CancellationToken.None);
+            var features = new FeatureCollection();
+            features.Set<IHttpRequestFeature>(new HttpRequestFeature());
+            features.Set<IHttpResponseFeature>(new HttpResponseFeature());
+            features.Set<IHttpResponseBodyFeature>(new PipeResponseBodyFeature(pipe.Writer));
+
+            var context = new DefaultHttpContext(features);
             context.Request.Method = HttpMethods.Get;
             context.Request.Path = "/b";
 
             await _middleware.Invoke(context);
+            await pipe.Writer.CompleteAsync();
+            await consumer;
+        }
+
+        private async ValueTask ConsumeAsync(PipeReader reader, CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                var result = await reader.ReadAsync(cancellationToken);
+                var buffer = result.Buffer;
+
+                reader.AdvanceTo(buffer.End, buffer.End);
+
+                if (result.IsCompleted)
+                {
+                    break;
+                }
+            }
+
+            await reader.CompleteAsync();
+        }
+
+        private class PipeResponseBodyFeature : IHttpResponseBodyFeature
+        {
+            public PipeResponseBodyFeature(PipeWriter pipeWriter)
+            {
+                Writer = pipeWriter;
+            }
+
+            public Stream Stream => Writer.AsStream();
+
+            public PipeWriter Writer { get; }
+
+            public Task CompleteAsync() => Writer.CompleteAsync().AsTask();
+
+            public void DisableBuffering()
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task SendFileAsync(string path, long offset, long? count, CancellationToken cancellationToken = default)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task StartAsync(CancellationToken cancellationToken = default)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
