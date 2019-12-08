5ab445149b8b7484d7daa4fcf34e6271c0b69479
Alessio Franceschelli <alessio@franceschelli.me>
2019-12-08 00:44:48 +0000

    updated benchmark

--- src/Middleware/perf/ResponseCaching.Microbenchmarks/ResponseCachingBenchmark.cs
+++ src/Middleware/perf/ResponseCaching.Microbenchmarks/ResponseCachingBenchmark.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
 using Microsoft.AspNetCore.Http;
@@ -20,24 +19,14 @@ namespace Microsoft.AspNetCore.WebSockets.Microbenchmarks
         private static readonly string _cacheControl = $"{CacheControlHeaderValue.PublicString}, {CacheControlHeaderValue.MaxAgeString}={int.MaxValue}";
 
         private ResponseCachingMiddleware _middleware;
-        //private readonly MemoryStream _memory;
-        private readonly byte[] _data = new byte[100 * 1024];
+        private readonly byte[] _data = new byte[100 * 1024 * 1024];
 
-        [Params(10, 1024, 100*1024)]
+        [Params(
+            100,
+            64 * 1024,
+            100 * 1024 * 1024
+        )]
         public int Size { get; set; }
-
-        public IEnumerable<string> CacheControlValues => new[] {
-            $"{CacheControlHeaderValue.PublicString}, {CacheControlHeaderValue.MaxAgeString}={int.MaxValue}",
-            $"{CacheControlHeaderValue.PublicString}, {CacheControlHeaderValue.MaxAgeString}={int.MaxValue}"
-        };
-
-        [ParamsSource(nameof(CacheControlValues))]
-        public string CacheControl { get; set; }
-
-        public ResponseCachingBenchmark()
-        {
-            //_memory = new MemoryStream();
-        }
 
         [GlobalSetup]
         public void Setup()
@@ -45,11 +34,12 @@ namespace Microsoft.AspNetCore.WebSockets.Microbenchmarks
             _middleware = new ResponseCachingMiddleware(
                     async context => {
                         context.Response.Headers[HeaderNames.CacheControl] = _cacheControl;
-                        await context.Response.BodyWriter.WriteAsync(_data.AsMemory(0, Size));
+                        await context.Response.BodyWriter.WriteAsync(new ReadOnlyMemory<byte>(_data, 0, Size));
                     },
                     Options.Create(new ResponseCachingOptions
                     {
-                        SizeLimit = int.MaxValue // ~2GB
+                        SizeLimit = int.MaxValue, // ~2GB
+                        MaximumBodySize = int.MaxValue, // ~2GB
                     }),
                     NullLoggerFactory.Instance,
                     new DefaultObjectPoolProvider()
@@ -58,31 +48,27 @@ namespace Microsoft.AspNetCore.WebSockets.Microbenchmarks
             // no need to actually cache as there is a warm-up fase
         }
 
-        [Benchmark]
-        public async Task Cache()
-        {
-            var context = new DefaultHttpContext();
-            context.Request.Method = HttpMethods.Get;
-            context.Request.Path = "/a";
+        //[Benchmark]
+        //public async Task Cache()
+        //{
+        //    var context = new DefaultHttpContext();
+        //    context.Request.Method = HttpMethods.Get;
+        //    context.Request.Path = "/a";
 
-            // don't serve from cache but store result
-            context.Request.Headers[HeaderNames.CacheControl] = CacheControlHeaderValue.NoCacheString;
+        //    // don't serve from cache but store result
+        //    context.Request.Headers[HeaderNames.CacheControl] = CacheControlHeaderValue.NoCacheString;
 
-            await _middleware.Invoke(context);
-        }
+        //    await _middleware.Invoke(context);
+        //}
 
         [Benchmark]
         public async Task ServeFromCache()
         {
-            //_memory.Seek(0, SeekOrigin.Begin);
             var context = new DefaultHttpContext();
-            //context.Response.Body = _memory;
             context.Request.Method = HttpMethods.Get;
             context.Request.Path = "/b";
 
             await _middleware.Invoke(context);
-
-            context.Response.BodyWriter.Complete();
         }
     }
 }
